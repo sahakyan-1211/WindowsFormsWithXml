4b71e71fed0511cf8fe1c05db4f1ff808d3affe7
pchristie.1996@gmail.com <kisuna181996>
2021-05-23 22:18:58 +0400

    changed button and form names

---
+++ WinForms/EmployeeForm.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace WinForms
+    {
+    partial class EmployeeForm
+        {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose (bool disposing)
+            {
+            if ( disposing && ( components != null ) )
+                {
+                components.Dispose ();
+                }
+            base.Dispose (disposing);
+            }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent ()
+            {
+            this.employeeDataGridView = new System.Windows.Forms.DataGridView();
+            this.Insert = new System.Windows.Forms.Button();
+            this.Update = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.employeeDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // employeeDataGridView
+            // 
+            this.employeeDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.employeeDataGridView.Location = new System.Drawing.Point(112, 47);
+            this.employeeDataGridView.Name = "employeeDataGridView";
+            this.employeeDataGridView.Size = new System.Drawing.Size(273, 172);
+            this.employeeDataGridView.TabIndex = 0;
+            // 
+            // Insert
+            // 
+            this.Insert.Location = new System.Drawing.Point(112, 225);
+            this.Insert.Name = "Insert";
+            this.Insert.Size = new System.Drawing.Size(75, 23);
+            this.Insert.TabIndex = 1;
+            this.Insert.Text = "Insert";
+            this.Insert.UseVisualStyleBackColor = true;
+            // 
+            // Update
+            // 
+            this.Update.Location = new System.Drawing.Point(210, 225);
+            this.Update.Name = "Update";
+            this.Update.Size = new System.Drawing.Size(75, 23);
+            this.Update.TabIndex = 2;
+            this.Update.Text = "Update";
+            this.Update.UseVisualStyleBackColor = true;
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(310, 225);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(75, 23);
+            this.Delete.TabIndex = 3;
+            this.Delete.Text = "Delete";
+            this.Delete.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.Update);
+            this.Controls.Add(this.Insert);
+            this.Controls.Add(this.employeeDataGridView);
+            this.Name = "EmployeeForm";
+            this.Text = "EmployeeForm";
+            ((System.ComponentModel.ISupportInitialize)(this.employeeDataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+            }
+
+        #endregion
+        private System.Windows.Forms.DataGridView employeeDataGridView;
+        private System.Windows.Forms.Button Insert;
+        private System.Windows.Forms.Button Update;
+        private System.Windows.Forms.Button Delete;
+        }
+    }
+
--- WinForms/Program.cs
+++ WinForms/Program.cs
@@ -5,18 +5,18 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WinForms
-    {
+{
     static class Program
-        {
+    {
         /// <summary>
         /// Главная точка входа для приложения.
         /// </summary>
         [STAThread]
-        static void Main ()
-            {
-            Application.EnableVisualStyles ();
-            Application.SetCompatibleTextRenderingDefault (false);
-            Application.Run (new Form1 ());
-            }
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new EmployeeForm());
         }
     }
+}
